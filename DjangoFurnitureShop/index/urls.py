cf1ea4a903644dbaac6a0da5009055b8496e0d8c
PrzemyslawSarnacki <przemos100@gmail.com>
2019-12-08 13:41:59 +0100

    Added Detail View

--- DjangoFurnitureShop/index/urls.py
+++ DjangoFurnitureShop/index/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
+
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.index, name=''),
-    path('<int:id>', views.index, name=''),
+    path('', views.product_list, name='product_list'),
+    path('product_detail/<pk>/', views.product_detail, name='product_detail'),
+    path('<int:id>', views.product_list, name=''),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Create your tests here.
+# Create your tests here.  
+ 
--- DjangoFurnitureShop/index/views.py
+++ DjangoFurnitureShop/index/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from django.contrib.auth.models import User
 from .models import Product, Comment
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
 
 
 # def product_list(request):
@@ -13,14 +12,11 @@ from django.views.decorators.debug import sensitive_post_parameters
 #     users = User.objects.all()
 #     return render(request, 'index/product_list.html', {'products': products, 'users': users})
 
-# def a_post_list(request, pk):
-# 	products = Product.objects.filter(author=pk)
-# 	return render(request, 'index/a_product_list.html', {'products': products})
 
 # @login_required
-# def post_new(request):
-#     if request.method == "POST":
-#         form = ProductForm(request.POST)
+# def product_new(request):
+#     if request.method == "product":
+#         form = ProductForm(request.product)
 #         if form.is_valid():
 #             product = form.save(commit=False)
 #             product.author = request.user
@@ -31,9 +27,16 @@ from django.views.decorators.debug import sensitive_post_parameters
 #         form = ProductForm()
 #     return render(request, 'index/product_edit.html', {'form': form})
 
-def index(request):
+def product_list(request):
     products = Product.objects.all()
     users = User.objects.all()
     return render(request, 'index/product_list.html', {'products': products, 'users': users})
 
+def a_product_list(request, pk):
+	products = Product.objects.filter(manufacturer=pk)
+	return render(request, 'index/a_product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'index/product_detail.html', {'product': product})
 # Create your views here.
